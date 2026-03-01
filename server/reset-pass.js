81a779ddfff2542f70f8f65298c43a4842deddb7
Tej Praval <tejpraval32@gmail.com>
2026-03-01 20:05:32 +0530

    unpublic the ulr of db

--- server/reset-pass.js
+++ server/reset-pass.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+import { ENV } from './env';
 
 async function run() {
-    await mongoose.connect('mongodb+srv://tejpraval:UrKTIZQLvMlJW9E6@cluster0.0xs5bzs.mongodb.net/tenant-db');
+    await mongoose.connect(ENV.MONGO_URI);
     console.log('Connected');
     const hash = await bcrypt.hash('password', 10);
     await mongoose.connection.db.collection('users').updateOne({ email: 'admin@test.com' }, { $set: { password: hash } });
